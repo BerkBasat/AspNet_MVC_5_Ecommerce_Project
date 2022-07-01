21ebae539b5e34c1aa79ec7d04bf5decc919d912
BerkBasat <berk.basat@bilgeadamakademi.com>
2022-07-01 19:43:27 +0300

    Mail sender added

---
+++ Technomarkt_Ecommerce/UI/Models/ContactVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models
+{
+    public class ContactVM
+    {
+        [Required(ErrorMessage = "Email is Required!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Message is Required!")]
+        public string Message { get; set; }
+    }
+}
