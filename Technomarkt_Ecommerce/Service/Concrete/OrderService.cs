fa506707f0617728d0a98ef396a9700d3ff8a2fc
BerkBasat <berk.basat@bilgeadamakademi.com>
2022-07-16 18:10:34 +0300

    Order service updated.

--- Technomarkt_Ecommerce/Service/Concrete/OrderService.cs
+++ Technomarkt_Ecommerce/Service/Concrete/OrderService.cs
@@ -21,5 +21,69 @@ namespace Service.Concrete
 
             return result;
         }
+
+        //Change order status to Shipping
+        public string ShipOrder(Guid id)
+        {
+            try
+            {
+                Order shipped = GetById(id);
+                shipped.OrderStatus = DAL.Enums.OrderStatus.Shipping;
+                Update(shipped);
+                return "Order sent to cargo!";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        //Change order status to Delivered
+        public string DeliverOrder(Guid id)
+        {
+            try
+            {
+                Order delivered = GetById(id);
+                delivered.OrderStatus = DAL.Enums.OrderStatus.Delivered;
+                Update(delivered);
+                return "Order delivered!";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        //Change order status to Cancelled
+        public string CancelOrder(Guid id)
+        {
+            try
+            {
+                Order cancelled = GetById(id);
+                cancelled.OrderStatus = DAL.Enums.OrderStatus.Cancelled;
+                Update(cancelled);
+                return "Order cancelled!";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        //Change order status to Refunded
+        public string RefundOrder(Guid id)
+        {
+            try
+            {
+                Order refunded = GetById(id);
+                refunded.OrderStatus = DAL.Enums.OrderStatus.Refunded;
+                Update(refunded);
+                return "Order refunded!";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
     }
 }
