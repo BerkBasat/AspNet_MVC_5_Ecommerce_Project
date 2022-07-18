1d9e27cae7f14cfb7ac0bf66f4217f2f66490ea6
BerkBasat <berk.basat@bilgeadamakademi.com>
2022-07-18 16:27:28 +0300

    Admin area updated

---
+++ Technomarkt_Ecommerce/UI/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using Service.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class OrderController : Controller
+    {
+        OrderService orderService = new OrderService();
+
+        public ActionResult Index()
+        {
+            return View(orderService.GetList());
+        }
+    }
+}
