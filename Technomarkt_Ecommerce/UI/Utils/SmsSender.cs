49a760a8db65e6593600ad8ee1b0ef63d39ceeab
BerkBasat <berk.basat@bilgeadamakademi.com>
2022-07-28 16:03:50 +0300

    SMS sender added and tested.

---
+++ Technomarkt_Ecommerce/UI/Utils/SmsSender.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace UI.Utils
+{
+    public class SmsSender
+    {
+        public static string SendSms(string msg, string phoneNumber)
+        {
+            var accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID"); ;
+            var authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var to = new PhoneNumber(phoneNumber);
+            var from = new PhoneNumber("+12393560151");
+
+            var message = MessageResource.Create(
+                to: to,
+                from: from,
+                body: msg);
+
+            return message.Sid;
+
+        }
+    }
+}
